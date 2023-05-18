b4850fab75a7c4315e7583b4c143637e9bcea07e
Mclean Kasambala <mcleankasambala@gmail.com>
2023-05-18 18:55:50 +0200

    added filables

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,22 @@
 class Goal < ApplicationRecord
-  belongs_to :categories
-  belongs_to :statuses
+	include ActionView::Helpers::NumberHelper
+
+  belongs_to :category
+  belongs_to :status
+
+  has_many :goal_progress
+
+  validates_presence_of :title, :amount, :category_id, :description, :start_date, :end_date, :status_id
+
+  def formatted_amount
+    number_to_currency(amount, unit: "MK", separator: ".", delimiter: ",")
+  end
+
+  def formatted_start_date
+    start_date.strftime("%d %b, %Y")
+  end
+
+  def formatted_end_date
+    end_date.strftime("%d %b, %Y")
+  end
 end
