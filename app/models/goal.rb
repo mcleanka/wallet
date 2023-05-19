1fa527d6f1251fc6af8e063163c8d98429eaa0a6
Mclean Kasambala <mcleankasambala@gmail.com>
2023-05-19 17:34:15 +0200

    fixed model relations

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,12 +1,13 @@
 class Goal < ApplicationRecord
 	include ActionView::Helpers::NumberHelper
+	include ActionView::Helpers::DateHelper
 
   belongs_to :category
   belongs_to :status
 
   has_many :goal_progress
 
-  validates_presence_of :title, :amount, :category_id, :description, :start_date, :end_date, :status_id
+  validates_presence_of :title, :amount, :category_id, :start_date, :end_date, :status_id
 
   def formatted_amount
     number_to_currency(amount, unit: "MK", separator: ".", delimiter: ",")
@@ -19,4 +20,8 @@ class Goal < ApplicationRecord
   def formatted_end_date
     end_date.strftime("%d %b, %Y")
   end
+
+  def duration
+		distance_of_time_in_words(start_date, end_date, include_seconds: true)
+	end
 end
