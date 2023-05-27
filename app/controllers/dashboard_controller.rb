aa73f3390bf1b068bd0c359c807e01154955dd2f
Mclean Kasambala <mcleankasambala@gmail.com>
2023-05-27 08:04:33 +0200

    included colors

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,13 +2,24 @@ class DashboardController < ApplicationController
 	include ActionView::Helpers::NumberHelper
 
   def index
-    loan_amount_sum = Loan.sum(:amount)
+    loan_amount_sum = Loan.joins(:status)
+                      .where(statuses: { name: 'active' })
+                      .sum(:amount)
+
     interest_sum = Loan.pluck(:interest_rate).sum
     @total_loan = (loan_amount_sum + interest_sum)
+    @total_loan_payments = LoanPayment.sum(:amount)
 
     @total_income = Income.sum(:amount)
     @total_expense = Expense.sum(:amount)
-    @total_budget = Budget.total_amount_within_month_duration
+    
+    @total_budget = Budget.joins(:status)
+                      .where(statuses: { name: 'pending' })
+                      .total_amount_within_month_duration
+
+    total_deductions_amount = @total_loan_payments + @total_expense
+
+    @total_income = @total_income - total_deductions_amount
 
     @formatted_total_loan = number_to_currency(@total_loan, unit: "MK", separator: ".", delimiter: ",")
     @formatted_total_income = number_to_currency(@total_income, unit: "MK", separator: ".", delimiter: ",")
